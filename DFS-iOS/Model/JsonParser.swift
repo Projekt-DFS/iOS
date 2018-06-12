01a1e3a3a9681de2b0c38e42e5bb9924576c208f
Julian Einspenner <julianeinspenner@yahoo.de>
2018-06-12 21:23:08 +0200

    JSON-Parser fuer die LoginResponse: Von JSON nach Image

--- DFS-iOS/Model/JsonParser.swift
+++ DFS-iOS/Model/JsonParser.swift
@@ -10,22 +10,38 @@ import Foundation
 
 class JsonParser{
     
-    //rueckgabeWert fehlt noch
-    static func parseFromJsonToMetaData(data: Data) {
-    
-        let jsonDataExtracted = extractJsonData(data: data)
-        //let metaData = MetaData.init(json: jsonDataExtracted)
+    static func parseFromJsonToImage(data: Data) -> Image{
+        let json = extractJsonData(data)
         
-        // return metaData sobald Bennis Klasse steht
+        let img = ImageStruct(json: json)
         
+        var ownerForMeta = "", createdForMeta = "", locationForMeta = ""
+        var tagListForMeta = [""]
+        
+        if let owner = img.metaData["owner"] as? String {
+            ownerForMeta = owner
+        }
+        if let created   = img.metaData["created"] as? String {
+            createdForMeta = created
+        }
+        if let location  = img.metaData["location"] as? String{
+            locationForMeta = location
+        }
+        if let tagList   = img.metaData["tagList"]  as? [String]{
+            tagListForMeta = tagList
+        }
+        
+        let metaData = MetaData(owner: ownerForMeta, created: createdForMeta, location: locationForMeta, tagList: tagListForMeta)
+        
+        return Image(id: img.id, imageSource: img.imageSource, thumbnail: img.thumbnail, metaData: metaData)
     }
     
     
-    static func extractJsonData(data: Data) -> [String: Any] {
-    
+    static func extractJsonData(_ data: Data) -> [String: Any] {
+        
         do{
             guard let jsonData = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {
-                return ["":""] //geht bestimmt klueger... mal sehen
+                return ["":""]
             }
             return jsonData
         }catch let jsonErr{
@@ -39,7 +55,7 @@ class JsonParser{
 
 //-----Structs-----//
 
-//fuer die Methode: parseFromJsonToMetaData
+//mal sehen, ob wir das ueberhaupt noch brauchen
 struct MetaDataStruct{
     let owner    : String
     let created  : String
@@ -53,3 +69,28 @@ struct MetaDataStruct{
         tagList   =  json["tagList" ]  as? String ?? ""
     }
 }
+
+//Damit koennen wir das komplette Image-Objekt erstellen
+struct ImageStruct{
+    let id          : Int
+    let imageSource : String
+    let thumbnail   : String
+    let metaData    : [String: Any]
+    
+    
+    init(json: [String: Any]){
+        id            =  json["id"   ]          as?  Int             ??  -1
+        imageSource   =  json["imageSource" ]   as?  String          ??  ""
+        thumbnail     =  json["thumbnail"]      as?  String          ??  ""
+        metaData      =  json["metaData" ]      as?  [String: Any]   ??  ["":""]
+    }
+    
+}
+
+
+
+
+
+
+
+
